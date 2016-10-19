ea9d5bf2443740ffb5c5e467735223467197c093
Evgeny Demchenko <little_pea@list.ru>
2016-10-19 17:18:17 +0800

    update slides

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
-jupyter-nbconvert --to slides refactoring.ipynb
 git commit -a -m "update slides"
 
 git checkout -B gh-pages
+jupyter-nbconvert --to slides refactoring.ipynb
 mv refactoring.slides.html index.html
 git commit -a -m "update pages"
-git push origin master gh-pages
+git pull origin master gh-pages && git push origin master gh-pages
 
 git checkout master
 rm -rf index.html
-git push origin master
+git pull origin master && git push origin master
